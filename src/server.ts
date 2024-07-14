0cdc8e8e2388eb30fa7dd88d27440f6afe1d486d
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-14 10:38:01 -0300

    feat: init application

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+// GET, POST, PUT, PATCH, DELETE
+
+// http://localhost:3333/hello
+
+
+app.get('/hello', (req, res) => {
+  return 'Hello Dev';
+});
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running!')
+});
