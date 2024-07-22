807d7acd78318daddf21a24f944e8c787d922e8d
Hudson Holanda <hudsonholanda55@gmail.com>
2024-07-22 10:29:15 -0300

    feat: create test to register new transaction

---
+++ test/example.spec.ts
@@ -0,0 +1,23 @@
+import { expect, beforeAll, afterAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
